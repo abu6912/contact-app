3d4e92d0f51e04332e3b825512d94119dedf03d1
abu6912 <abu6912@gmail.com>
2021-12-29 21:08:10 +0530

    add images in React

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      "name": "Deep",
+      "email": "deep@gm",
+    },
+    {
+      id: "2",
+      "name": "abu",
+      "email": "abu@gm",
+    },
+  ];
   return (
-    <div>Hello World</div>
+    <div className='ui container'>
+      <Header />
+      <AddContact />
+      <ContactList contacts = {contacts}/>
+    </div>
   );
 }
 
